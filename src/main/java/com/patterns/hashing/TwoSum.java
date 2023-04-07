653e3077f0c964b2d17725f9171b2c6b0fb3822d
miladra <miladranjbari@gmail.com>
2023-04-07 13:06:39 +0330

    Add Hashing

--- src/main/java/com/patterns/hashing/TwoSum.java
+++ src/main/java/com/patterns/hashing/TwoSum.java
@@ -1,8 +1,12 @@
 package com.patterns.hashing;
 
+import org.junit.jupiter.api.Test;
+
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 class SolutionBruteForce {
     // time: O(N*N)
@@ -49,5 +53,27 @@ class SolutionLambda{
                 )
                 .findFirst()
                 .orElse(new int[]{});
+    }
+}
+
+
+class SolutionTest {
+    @Test
+    public void testTwoSum() {
+        Solution solution = new Solution();
+        int[] nums1 = {2, 7, 11, 15};
+        int target1 = 9;
+        int[] result1 = solution.twoSum(nums1, target1);
+        assertArrayEquals(new int[]{0, 1}, result1);
+
+        int[] nums2 = {3, 2, 4};
+        int target2 = 6;
+        int[] result2 = solution.twoSum(nums2, target2);
+        assertArrayEquals(new int[]{1, 2}, result2);
+
+        int[] nums3 = {3, 3};
+        int target3 = 6;
+        int[] result3 = solution.twoSum(nums3, target3);
+        assertArrayEquals(new int[]{0, 1}, result3);
     }
 }
