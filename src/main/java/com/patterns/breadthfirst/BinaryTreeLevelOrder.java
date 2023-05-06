91df7d89db615c1d6a70266a67858e3a71b87ef7
miladra <miladranjbari@gmail.com>
2023-05-07 01:36:50 +0330

    Refactor BinaryTreeLevelOrder

--- src/main/java/com/patterns/breadthfirst/BinaryTreeLevelOrder.java
+++ src/main/java/com/patterns/breadthfirst/BinaryTreeLevelOrder.java
@@ -6,34 +6,6 @@ import java.util.List;
 import java.util.Queue;
 
 public class BinaryTreeLevelOrder {
-        public List<List<Integer>> levelOrder2(TreeNode root) {
-
-            List<List<Integer>> res = new ArrayList<>();
-
-            if(root == null) return res;
-
-            Queue<TreeNode> queue = new LinkedList<>();
-            queue.add(root);
-
-            while(!queue.isEmpty()){
-                List<Integer> level = new ArrayList<>();
-                int cnt = queue.size();
-                for(int i= 0; i < cnt; i++){
-
-                    TreeNode node = queue.poll();
-                    level.add(node.val);
-
-                    if(node.left != null) queue.add(node.left);
-
-                    if(node.right != null) queue.add(node.right);
-
-                }
-
-                res.add(level);
-            }
-
-            return res;
-        }
         public List<List<Integer>> levelOrder(TreeNode root) {
 
             List<List<Integer>> result = new ArrayList<List<Integer>>();
@@ -81,4 +53,33 @@ public class BinaryTreeLevelOrder {
 
             return result;
         }
+
+    public List<List<Integer>> levelOrder2(TreeNode root) {
+
+        List<List<Integer>> res = new ArrayList<>();
+
+        if(root == null) return res;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        while(!queue.isEmpty()){
+            List<Integer> level = new ArrayList<>();
+            int cnt = queue.size();
+            for(int i= 0; i < cnt; i++){
+
+                TreeNode node = queue.poll();
+                level.add(node.val);
+
+                if(node.left != null) queue.add(node.left);
+
+                if(node.right != null) queue.add(node.right);
+
+            }
+
+            res.add(level);
+        }
+
+        return res;
+    }
 }
